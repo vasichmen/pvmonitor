14cc988716aaf97883eb86cb26208514e1d6b9de
Василий Метт <v.mett@qsoft.ru>
2022-11-19 20:44:19 +0300

    Добавил очистку таблицы

--- database/migrations/2022_11_19_171739_add_random_field.php
+++ database/migrations/2022_11_19_171739_add_random_field.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\SolarInsolation;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,6 +13,8 @@ return new class extends Migration {
      */
     public function up()
     {
+        SolarInsolation::query()->forceDelete();
+
         Schema::table('solar_insolations', function (Blueprint $table) {
             $table->integer('random');
         });
